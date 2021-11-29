1629cb45612ec47ace69f0642d3fc548068b0651
William Hendershot <whendershot1@gmail.com>
2021-11-29 16:28:41 +0900

    Initial implementation

--- 4_function_calculator/script.js
+++ 4_function_calculator/script.js
@@ -4,7 +4,7 @@ const VALID_OPERATIONS = ["+", "-", "/", "*"];
 
 var operands = [];
 var current_operand = 0;
-var operation = "";
+var current_operation = "";
 
 function press(num) {
     let temp = current_operand.toString();
@@ -12,22 +12,48 @@ function press(num) {
     temp = parseInt(temp);
     current_operand = temp;
     console.log(`Adding: ${num}, Setting operand to ${temp}`);
+    
     update();
 }
 
 function setOP(op) {
     if (VALID_OPERATIONS.includes(op)) {
-        operation = op;        
-        console.log(`Setting operation to: ${op} `);
+        if (current_operation === "") {
+            current_operation = op;
+            operands.push(current_operand);
+            current_operand = 0;
+
+        }
+        console.log(`Setting operation to: ${op}`);
     }
+    
     update();
 }
 
 function calculate (){
+    let lhs = current_operand;
+    let rhs = operands.pop();
+
+    switch(current_operation) {
+        case "+":
+            current_operand = lhs + rhs;
+        case "-":
+            current_operand = lhs - rhs;
+        case "/":
+            current_operand = lhs / rhs;
+        case "*":
+            current_operand = lhs * rhs;
+    }
+
+    current_operation = "";
     update();
 }
 
 function clr () {
+    operands = [];
+    current_operand = 0;
+    current_operation = "";
+    
     update();
 }
 
